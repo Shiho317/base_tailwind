e3a9cf061da6846c7022f72f1f11a7d49be96527
Shiho Nagano <shiho@nagano.com>
2021-12-20 17:59:27 +0900

    complete-style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
+  mode: "jit",
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '128': '32rem',
+      },
+      boxShadow: {
+        '3xl': '0 0px 15px 0px rgba(0, 0, 0, 0.3)',
+      }
+    },
   },
   plugins: [],
 }
